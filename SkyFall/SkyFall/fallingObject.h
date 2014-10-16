5163a3766828107c0c46e9bb40afdaffe939dbb4
Josh Dee <joshdee@MAC020.local>
2014-10-16 10:19:42 +0800

    add falling object class

---
+++ SkyFall/SkyFall/fallingObject.h
@@ -0,0 +1,13 @@
+//
+//  fallingObject.h
+//  SkyFall
+//
+//  Created by Josh Dee on 10/16/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fallingObject : UIImage
+
+@end
---
+++ SkyFall/SkyFall/fallingObject.m
@@ -0,0 +1,13 @@
+//
+//  fallingObject.m
+//  SkyFall
+//
+//  Created by Josh Dee on 10/16/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import "fallingObject.h"
+
+@implementation fallingObject
+
+@end
